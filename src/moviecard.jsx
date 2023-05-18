2cf677388a5f28968996946b581aa3f1789050ff
Praneshbharadwaj <praneshbharadwaj6312gmail.com>
2023-05-18 18:53:58 +0530

    hi

--- src/moviecard.jsx
+++ src/moviecard.jsx
@@ -1,23 +1,23 @@
 import React from "react";
-const MovieCard = ({movie}) => {
+const MovieCard = ({movie1}) => {
   return (
     <div className="movie">
       <div>
-        <p>{movie.Year}</p>
+        <p>{movie1.Year}</p>
       </div>
       <div>
         <img
           src={
-            movie.Poster !== "N/A"
-              ? movie.Poster
+            movie1.Poster !== "N/A"
+              ? movie1.Poster
               : "https//via.placeholder.com/400"
           }
-          alt={movie.Title}
+          alt={movie1.Title}
         />
       </div>
       <div>
-        <span>{movie.Type}</span>
-        <h3>{movie.Title}</h3>
+        <span>{movie1.Type}</span>
+        <h3>{movie1.Title}</h3>
       </div>
     </div>
   );
